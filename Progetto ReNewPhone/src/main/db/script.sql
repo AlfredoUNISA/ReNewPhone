c9b99e84866bc1c448956fe6236945a427f2bf6b
Giuseppe Giardinetto <g.giardinetto@studenti.unisa.it>
2023-05-12 17:28:54 +0200

    Update script.sql

--- Progetto ReNewPhone/src/main/db/script.sql
+++ Progetto ReNewPhone/src/main/db/script.sql
@@ -4,6 +4,7 @@ USE storage;
 
 DROP TABLE IF EXISTS product;
 DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS buys;
 
 CREATE TABLE product (	
   id int primary key AUTO_INCREMENT,
@@ -16,7 +17,6 @@ CREATE TABLE product (
   category char(20)
 );
 
-
 CREATE TABLE user(
     id int primary key AUTO_INCREMENT,
     name char(20) not null,
@@ -26,6 +26,15 @@ CREATE TABLE user(
     city char(20) not null,
     cap int not null,
     phone int not null
+);
+
+CREATE TABLE buys(
+    id int primary key AUTO_INCREMENT,
+    id_user int not null,
+    id_product int not null,
+    quantity int not null,
+    FOREIGN KEY (id_user) REFERENCES user(id),
+    FOREIGN KEY (id_product) REFERENCES product(id)
 );
 
 INSERT INTO product (name, description, price, quantity, color, brand, category) VALUES
@@ -41,4 +50,21 @@ INSERT INTO product (name, description, price, quantity, color, brand, category)
 ('Lenovo Yoga Tab 13', 'The most powerful Yoga Tab ever.', 799, 100, 'Storm Grey', 'Lenovo', 'Tablet'),
 ('Asus ZenPad 13 8GB', 'The most powerful ZenPad ever.', 699, 100, 'Star Black', 'Asus', 'Tablet'),
 ('Huawei MatePad Pro 12.6', 'The most powerful MatePad ever.', 899, 100, 'Midnight Black', 'Huawei', 'Tablet'),
-('Honor MagicPad 10.4', 'The most powerful MagicPad ever.', 599, 100, 'Midnight Black', 'Honor', 'Tablet');
+('Honor MagicPad 10.4', 'The most powerful MagicPad ever.', 599, 100, 'Midnight Black', 'Honor', 'Tablet'),
+('Nokia XR20', 'The most durable smartphone ever.', 499, 100, 'Midnight Blue', 'Nokia', 'Smartphone'),
+('Motorola Moto G Power (2022)', 'The most affordable smartphone with a long-lasting battery.', 199, 100, 'Black', 'Motorola', 'Smartphone'),
+('TCL 20 Pro 5G', 'The most affordable 5G smartphone.', 299, 100, 'Moonlight Black', 'TCL', 'Smartphone'),
+('Oppo Reno 7 5G', 'The most stylish 5G smartphone.', 399, 100, 'Cosmic Black', 'Oppo', 'Smartphone'),
+('Vivo V23 5G', 'The most camera-centric 5G smartphone.', 499, 100, 'Sunshine Gold', 'Vivo', 'Smartphone'),
+('iQOO 9 5G', 'The most powerful 5G smartphone.', 599, 100, 'Legendary Edition', 'iQOO', 'Smartphone'),
+('Amazon Fire HD 10 (2021)', 'The most affordable 10-inch tablet.', 149, 100, 'Black', 'Amazon', 'Tablet'),
+('Lenovo Tab M10 Plus (2021)', 'The most powerful 10-inch tablet.', 249, 100, 'Storm Grey', 'Lenovo', 'Tablet'),
+('Asus ZenPad 10 4GB (2021)', 'The most affordable 10-inch tablet.', 199, 100, 'Star Black', 'Asus', 'Tablet'),
+('Huawei MatePad T10s (2021)', 'The most powerful 10-inch tablet.', 199, 100, 'Midnight Black', 'Huawei', 'Tablet'),
+('Honor MagicPad 10 (2021)', 'The most affordable 10-inch tablet.', 149, 100, 'Midnight Black', 'Honor', 'Tablet'),
+('Google Pixel 6a', 'The most affordable Pixel ever.', 499, 100, 'Sorta Seafoam', 'Google', 'Smartphone'),
+('OnePlus Nord CE 2 Lite 5G', 'The most affordable 5G smartphone.', 249, 100, 'Black', 'OnePlus', 'Smartphone'),
+('Realme 9 Pro 5G', 'The most powerful 5G smartphone.', 349, 100, 'Midnight Black', 'Realme', 'Smartphone'),
+('Motorola Moto G Stylus 5G (2022)', 'The most affordable smartphone with a stylus.', 299, 100, 'Black', 'Motorola', 'Smartphone'),
+('TCL 20 SE 5G', 'The most affordable 5G smartphone with a large display.', 249, 100, 'Moonlight Black', 'TCL', 'Smartphone'),
+('Oppo A96 5G', 'The most stylish 5G smartphone.', 399, 100, 'Cosmic Black', 'Oppo', 'Smartphone');
