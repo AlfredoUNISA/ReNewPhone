eff94660f815fdd45949f8cb8465cb0ab743356c
AlfredoUNISA <a.spera28@studenti.unisa.it>
2023-05-17 18:56:08 +0200

    Inizio lavoro sul carrello

---
+++ Progetto ReNewPhone/src/main/java/rnp/CartBean.java
@@ -0,0 +1,51 @@
+package rnp;
+
+import java.io.Serializable;
+
+/**
+ * Bean che contiene tutte le informazioni di un carrello.
+ * @category Bean
+ * @category MODIFICABILE (tutto)
+ */
+public class CartBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	int id_user;
+    int id_product;
+    int quantity;
+    
+	public CartBean() {
+		id_user = -1;
+		id_product = -1;
+		quantity = 0;
+	}
+
+	public int getId_user() {
+		return id_user;
+	}
+	public void setId_user(int id_user) {
+		this.id_user = id_user;
+	}
+
+	public int getId_product() {
+		return id_product;
+	}
+	public void setId_product(int id_product) {
+		this.id_product = id_product;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	@Override
+	public String toString() {
+		return "CartBean [id_user=" + id_user + ", id_product=" + id_product + ", quantity=" + quantity + "]";
+	}
+
+
+}
---
+++ Progetto ReNewPhone/src/main/java/rnp/CartDAODataSource.java
@@ -0,0 +1,325 @@
+package rnp;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.Collection;
+import java.util.LinkedList;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+/**
+ * Fornisce l'accesso ai dati di un oggetto Bean in una base di dati relazionale
+ * attraverso un pool di connessioni DataSource. La classe si occupa di eseguire
+ * le operazioni CRUD (create, retrieve, update e delete) sui dati nella tabella
+ * "TABLE_NAME" della base di dati.
+ * 
+ * @category Query con Data Source
+ * @implNote ATTENZIONE: Modificare web.xml (resource-ref con JNDI) e modificare
+ *           context.xml (in META-INF)
+ * @category MODIFICABILE
+ */
+public class CartDAODataSource implements IBeanDAO<CartBean> /* MODIFICABILE */ {
+
+	private static DataSource ds;
+	private static final String TABLE_NAME = "carts"; // MODIFICABILE
+
+	// Inizializzazione per il Data Source
+	static {
+		try {
+			Context initCtx = new InitialContext();
+			Context envCtx = (Context) initCtx.lookup("java:comp/env");
+
+			ds = (DataSource) envCtx.lookup("jdbc/renewphonedb"); // MODIFICABILE
+
+		} catch (NamingException e) {
+			System.out.println("Error:" + e.getMessage());
+		}
+	}
+
+	/**
+	 * Un bean viene inserito come una nuova riga nella tabella "TABLE_NAME" usando
+	 * una connessione al database.
+	 * 
+	 * @param cart_row Oggetto da inserire
+	 * @category MODIFICABILE
+	 */
+	@Override
+	public synchronized void doSave(CartBean cart_row) throws SQLException {
+
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+
+		// MODIFICABILE
+		String insertSQL = "INSERT INTO " + CartDAODataSource.TABLE_NAME + " (id_user, id_product, quantity) VALUES (?, ?, ?)";
+
+		try {
+			connection = ds.getConnection();
+
+			preparedStatement = connection.prepareStatement(insertSQL);
+
+			// MODIFICABILE
+			preparedStatement.setInt(1, cart_row.getId_user());
+			preparedStatement.setInt(2, cart_row.getId_product());
+			preparedStatement.setInt(3, cart_row.getQuantity());
+
+			preparedStatement.executeUpdate();
+
+			connection.commit();
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				if (connection != null)
+					connection.close();
+			}
+		}
+	}
+
+	/**
+	 * Rimuove UNA SINGOLA riga dalla tabella "TABLE_NAME" in base ad entrambe le
+	 * chiavi primarie.
+	 * 
+	 * @param id_user    Il codice UTENTE della riga da rimuovere dalla tabella.
+	 * @param id_product Il codice PRODOTTO della riga da rimuovere dalla tabella.
+	 * @return L'esito della query.
+	 * @category MODIFICABILE
+	 */
+	public synchronized boolean doDeleteSingleRow(int id_user, int id_product /* MODIFICABILE */) throws SQLException {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+
+		int result = 0;
+
+		String deleteSQL = "DELETE FROM " + CartDAODataSource.TABLE_NAME + " WHERE id_user = ? AND id_product = ?"; // MODIFICABILE
+
+		try {
+			connection = ds.getConnection();
+			preparedStatement = connection.prepareStatement(deleteSQL);
+
+			// MODIFICABILE
+			preparedStatement.setInt(1, id_user);
+			preparedStatement.setInt(2, id_product);
+
+			result = preparedStatement.executeUpdate();
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				if (connection != null)
+					connection.close();
+			}
+		}
+		return (result != 0);
+	}
+
+	/**
+	 * !!! ATTENZIONE !!! Elimina tutte le righe dalla tabella "TABLE_NAME" in base
+	 * all'id dell'UTENTE. Utilizzare la doDeleteSingleRow() se si vuole eliminare
+	 * una sola riga.
+	 * 
+	 * @param id_user Il codice UTENTE della riga da rimuovere dalla tabella.
+	 * @return L'esito della query.
+	 * @category MODIFICABILE
+	 */
+	@Override
+	public synchronized boolean doDelete(int id_user) throws SQLException {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+
+		int result = 0;
+
+		String deleteSQL = "DELETE FROM " + CartDAODataSource.TABLE_NAME + " WHERE id_user = ?"; // MODIFICABILE
+
+		try {
+			connection = ds.getConnection();
+			preparedStatement = connection.prepareStatement(deleteSQL);
+
+			// MODIFICABILE
+			preparedStatement.setInt(1, id_user);
+
+			result = preparedStatement.executeUpdate();
+
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				if (connection != null)
+					connection.close();
+			}
+		}
+		return (result != 0);
+	}
+
+	/**
+	 * Seleziona TUTTE le righe dalla tabella "TABLE_NAME" e restituisce una
+	 * collezione di oggetti.
+	 * 
+	 * @param order Specifica l'ordine di ordinamento dei risultati (se non è nullo
+	 *              aggiunge ORDER BY alla query).
+	 * @return La collezione di oggetti contenente tutte le righe della tabella.
+	 * @category MODIFICABILE
+	 */
+	@Override
+	public synchronized Collection<CartBean> doRetrieveAll(String order) throws SQLException {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+
+		Collection<CartBean> carts = new LinkedList<CartBean>();
+
+		String selectSQL = "SELECT * FROM " + CartDAODataSource.TABLE_NAME;
+
+		if (order != null && !order.equals("")) {
+			selectSQL += " ORDER BY " + order;
+		}
+
+		try {
+			connection = ds.getConnection();
+			preparedStatement = connection.prepareStatement(selectSQL);
+
+			ResultSet rs = preparedStatement.executeQuery();
+
+			while (rs.next()) {
+				CartBean bean = new CartBean();
+
+				// MODIFICABILE
+				bean.setId_user(rs.getInt("id_user"));
+				bean.setId_product(rs.getInt("id_product"));
+				bean.setQuantity(rs.getInt("quantity"));
+
+				carts.add(bean);
+			}
+
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				if (connection != null)
+					connection.close();
+			}
+		}
+		return carts;
+	}
+
+	/**
+	 * Seleziona una SINGOLA riga dalla tabella "TABLE_NAME" in base ad entrambe le
+	 * chiavi primarie.
+	 * 
+	 * @param id_user    Il codice UTENTE di cui si vuole ottenere la riga.
+	 * @param id_product Il codice PRODOTTO di cui si vuole ottenere la riga.
+	 * @return Il bean ottenuto in base al codice.
+	 * @category MODIFICABILE
+	 */
+	public synchronized CartBean doRetrieveByPrimaryKeys(int id_user, int id_product /* MODIFICABILE */)
+			throws SQLException {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+
+		CartBean bean = new CartBean();
+
+		String selectSQL = "SELECT * FROM " + CartDAODataSource.TABLE_NAME + " WHERE id_user = ? AND id_product = ?"; // MODIFICABILE
+
+		try {
+			connection = ds.getConnection();
+			preparedStatement = connection.prepareStatement(selectSQL);
+
+			// MODIFICABILE
+			preparedStatement.setInt(1, id_user);
+			preparedStatement.setInt(2, id_product);
+
+			ResultSet rs = preparedStatement.executeQuery();
+			
+			while (rs.next()) {
+				// MODIFICABILE
+				bean.setId_user(rs.getInt("id_user"));
+				bean.setId_product(rs.getInt("id_product"));
+				bean.setQuantity(rs.getInt("quantity"));
+			}
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				if (connection != null)
+					connection.close();
+			}
+		}
+		return bean;
+	}
+
+	/**
+	 * Seleziona una COLLEZIONE di righe dalla tabella "TABLE_NAME" in base all'id
+	 * dell'UTENTE.
+	 * 
+	 * @param id_user Il codice UTENTE di cui si vogliono ottenere gli oggetti nel
+	 *                carrello.
+	 * @param order   Specifica l'ordine di ordinamento dei risultati (se non è
+	 *                nullo aggiunge ORDER BY alla query).
+	 * @return La collezione di oggetti contenente tutte le righe inerenti all'utente.
+	 * @category MODIFICABILE
+	 */
+	public synchronized Collection<CartBean> doRetrieveByUser(int id_user, String order) throws SQLException {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		Collection<CartBean> carts = new LinkedList<CartBean>();
+
+		String selectSQL = "SELECT * FROM " + CartDAODataSource.TABLE_NAME + " WHERE id_user = ?"; // MODIFICABILE
+
+		if (order != null && !order.equals("")) {
+			selectSQL += " ORDER BY " + order;
+		}
+		
+		try {
+			connection = ds.getConnection();
+			preparedStatement = connection.prepareStatement(selectSQL);
+
+			// MODIFICABILE
+			preparedStatement.setInt(1, id_user);
+			
+			ResultSet rs = preparedStatement.executeQuery();
+			
+			while (rs.next()) {
+				CartBean bean = new CartBean();
+				
+				// MODIFICABILE
+				bean.setId_user(rs.getInt("id_user"));
+				bean.setId_product(rs.getInt("id_product"));
+				bean.setQuantity(rs.getInt("quantity"));
+				
+				carts.add(bean);
+			}
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				if (connection != null)
+					connection.close();
+			}
+		}
+		return carts;
+	}
+
+	/**
+	 * !!! ATTENZIONE !!! Sono forzato dall'interfaccia a creare questo metodo.
+	 * Usare doRetrieveByPrimaryKeys() oppure doRetrieveByUser().
+	 * 
+	 * @return null ed un messaggio in sout.
+	 * @deprecated
+	 */
+	@Override
+	public synchronized CartBean doRetrieveByKey(int NON_USARE_QUESTO_METODO) throws SQLException {
+		System.out.println("Non usare questo metodo, usa doRetrieveByPrimaryKeys() oppure doRetrieveByUser()");
+		return null;
+	}
+
+}
