02764e68cab0fb9e078a7a63c35c93e971d08e1f
Davide <d.capricano@studenti.unisa.it>
2023-07-06 15:52:29 +0200

    Update productsFilter.js

--- Progetto ReNewPhone/src/main/webapp/js/productsFilter.js
+++ Progetto ReNewPhone/src/main/webapp/js/productsFilter.js
@@ -104,7 +104,7 @@ function loadMoreFilteredProducts(priceMin,priceMax,memoryMin,memoryMax,filterBr
 					'<p>Marca: ' + this.brand + '</p>' +
 					'<p>Anno: ' + this.year + '</p>' +
 					'<p>Prezzo: ' + priceValue + '</p>' +
-					'<p><a href="products?action=details&name=' + this.groupName + '">Dettagli</a></p>' +
+					'<a href="products?action=details&name=' + this.groupName + '"> <input type="button"  value="Dettagli">   </a>' +
 					'</div>' +
 					'</div>';
 
