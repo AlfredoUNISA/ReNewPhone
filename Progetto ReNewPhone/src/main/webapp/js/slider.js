8b155db83de86065f139d33190b93f6db060f0bd
Davide <d.capricano@studenti.unisa.it>
2023-07-10 20:49:57 +0200

    Update slider.js

--- Progetto ReNewPhone/src/main/webapp/js/slider.js
+++ Progetto ReNewPhone/src/main/webapp/js/slider.js
@@ -28,3 +28,60 @@ function prec(){
 setInterval(succ, 7000);
 
 
+function toggleAnswer(element) {
+      element.nextElementSibling.classList.toggle("show");
+    }
+    
+    // Aggiungi un gestore di eventi per catturare il click sulle stelle
+    const stars = document.querySelectorAll(".rating > span");
+    
+    stars.forEach(function(star, index) {
+      star.addEventListener("click", function() {
+        resetStars();
+        highlightStars(index);
+      });
+    });
+    
+    function resetStars() {
+      stars.forEach(function(star) {
+        star.innerHTML = "&#9734;";
+      });
+    }
+    
+    function highlightStars(index) {
+      for (let i = 0; i <= index; i++) {
+        stars[i].innerHTML = "&#9733;";
+      }
+    }
+    
+ function inviaValutazione() {
+      var rating = document.querySelector('input[name="rating"]:checked');
+      if (rating) {
+        var valutazione = rating.value;
+        // Puoi qui eseguire le operazioni desiderate con la valutazione selezionata
+        console.log("Valutazione:", valutazione);
+        alert("Grazie per la tua valutazione di " + valutazione + " stelle!");
+      } else {
+        alert("Per favore, seleziona una valutazione.");
+      }
+    }
+    
+document.addEventListener("DOMContentLoaded", function() {
+  var popupOverlay = document.querySelector(".popup-overlay");
+  var closePopup = document.querySelector(".close-popup");
+  var emailForm = document.querySelector("form");
+  var emailInput = document.querySelector("input[type='email']");
+
+  closePopup.addEventListener("click", function() {
+    popupOverlay.style.display = "none";
+  });
+
+  emailForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    var emailAddress = emailInput.value;
+    
+    alert("Iscrizione avvenuta con successo per l'indirizzo: " + emailAddress);
+    emailInput.value = ""; // Resetta il valore dell'input
+    popupOverlay.style.display = "none"; // Chiudi il popup
+  });
+});
